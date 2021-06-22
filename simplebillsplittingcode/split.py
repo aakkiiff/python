64b7255a2b5f47a20b27dc73369bbec3a5e22479
Mohammad Akif <84233647+akkif@users.noreply.github.com>
2021-06-23 03:03:13 +0600

    Create split.py

---
+++ simplebillsplittingcode/split.py
@@ -0,0 +1,7 @@
+print("welcome to the tip calculator")
+bill =int(input("how much you guys need to  pay\n"))
+people=int(input("how many guys are you?\n"))
+vat=int(input("how much is vat?"))
+
+net=(bill+(bill*vat)/100)/people
+print(f"per person will be paying: {net}$")
