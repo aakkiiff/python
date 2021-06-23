310acae641c065fbd9aa5ababd35003f6ff4b9c1
Mohammad Akif <84233647+akkif@users.noreply.github.com>
2021-06-23 16:00:39 +0600

    Create billsplitter.py

---
+++ simplebillsplittingcode V2/billsplitter.py
@@ -0,0 +1,20 @@
+print("welcome to the tip calculator")
+bill =int(input("how much you guys need to  pay\n"))
+people=int(input("how many guys are you?\n"))
+vat=int(input("how much is vat?\n"))
+name=[]
+thedic={}
+for i in range(people):
+    a=input(f"type the name of person number {i+1} ?")
+    name.append(a)
+
+i=0
+for p in name:
+    a=input(f"how much {name[i]} spent?\n")
+    thedic[name[i]]=int(a)
+    i+=1
+#perperson will be paying?
+for o,i in thedic.items():
+    print(f'{o} will be paying {i+(i*vat)/100}$')
+
+
