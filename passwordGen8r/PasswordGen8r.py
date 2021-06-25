6285363ed3a0b2c2fa49db4282da1e0a98dfa805
Mohammad Akif <84233647+akkif@users.noreply.github.com>
2021-06-26 01:10:16 +0600

    typo fix

--- passwordGen8r/PasswordGen8r.py
+++ passwordGen8r/PasswordGen8r.py
@@ -4,11 +4,11 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
+print("Welcome to the Password Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-platform = input("for which platform are you using this password?\n")
+platform = input("for which platform are you using this password my boy?\n")
 p=''
 for i in range(0,nr_letters):
   a=random.randint(1,26)
