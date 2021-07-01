af0e4de71edab57f426335495c427f54faa18631
Mohammad Akif <84233647+akkif@users.noreply.github.com>
2021-07-01 22:11:43 +0600

    Create PasswordGen8_Dcry-V2.py

---
+++ passwordGen8-V2/PasswordGen8_Dcry-V2.py
@@ -0,0 +1,29 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+            'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+pas=input("type your encrypted password\n")
+shift = int(input("Type Your Secret Shift Code\n"))
+shift = shift % 52
+
+
+def caesar(start_text, shift_amount):
+  end_text = ""
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position - shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+
+  print(f"Here's the Decrypted Password: {end_text}")
+
+
+
+caesar(start_text=pas, shift_amount=shift)
